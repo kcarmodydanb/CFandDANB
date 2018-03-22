4927917418c12e12ba5698bcc0711c1c6856f3b5
Unknown <soltbahta@gmail.com>
2018-03-22 10:37:09 -0500

    Auto stash before merge of "master" and "origin/master"

--- State_Reports/State_sql_query_for_tables.sql
+++ State_Reports/State_sql_query_for_tables.sql
@@ -1,4 +1,14 @@
-SELECT
-cyber_txt as
-Email, display_nm as
-Name, cust.street1 || ' ' || cust.street2 || ' ' || cust.street3 || ' ' || cust.city_nm || ', ' || cust.state_cd || ' ' || cust.postal_cd AS Address, NULL AS SCHOOL, EXAM_AUTHORIZATION as EXAM_ID, EXAM_CD as EXAM, GRADE AS TEST_STATUS FROM VW_DANB_EXM_EXAM_AND_SECTION vxm JOIN VW_DANB_CEN_CUST_CUSTOMER cust on vxm.cust_id = cust.cust_id where exam_cd = 'GC' and state_cd = 'OK' and rownum < 5
+SELECT  
+   cyber_txt as Email,
+   display_nm as Name,
+   cust.street1 || ' ' || cust.street2 || ' ' || cust.street3 || ' ' || cust.city_nm || ', ' || cust.state_cd || ' ' || cust.postal_cd AS Address, 
+   NULL AS SCHOOL,
+   EXAM_AUTHORIZATION as EXAM_ID, 
+   EXAM_CD as EXAM,
+   GRADE AS TEST_STATUS 
+FROM VW_DANB_EXM_EXAM_AND_SECTION vxm
+JOIN VW_DANB_CEN_CUST_CUSTOMER cust on vxm.cust_id = cust.cust_id
+where exam_cd = 'GC' and state_cd = 'OK'
+-- prompt user for parameter for start and end date... 
+and exam_taken_dt between :startDate and :endDate
+and rownum < 5
--- VW_DANB_CRT_CUSTOMER.sql
+++ VW_DANB_CRT_CUSTOMER.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FORCE VIEW "VW_DANB_CRT_CUSTOMER" ("CUST_ID", "CERT_TY", "CERT_DESCR", "SPEC_TY", "LEVEL_ID", "CERT_STATUS", "CURRENT_EXP_DATE", "CERT_SERNO", "CERTIFICATE_NO", "PERIOD_SERNO", "CERTIFIED_SINCE_DT", "CREATE_DT", "CREATE_BY", "LAST_UPDATE_DT", "LAST_UPDATE_BY") AS 
+CREATE OR REPLACE FORCE VIEW "VW_DANB_CRT_CUSTOMER" ("CUST_ID", "CERT_TY", "CERT_DESCR", "SPEC_TY", "LEVEL_ID", "CERT_STATUS", "CURRENT_EXP_DATE", "CERT_SERNO", "CERTIFICATE_NO", "PERIOD_SERNO", "CERTIFIED_SINCE_DT", "CREATE_DT", "CREATE_BY", "LAST_UPDATE_DT", "LAST_UPDATE_BY", "PRINTED_DT") AS 
   SELECT crtMast.cust_id, 
   crtMast.cert_ty, 
   crtCfg.DESCR, 
@@ -13,7 +13,9 @@ CREATE OR REPLACE FORCE VIEW "VW_DANB_CRT_CUSTOMER" ("CUST_ID", "CERT_TY", "CERT
   crtMast.create_dt,
   crtMast.create_by,
   crtMast.last_update_dt,
-  crtMast.last_update_by
+  crtMast.last_update_by,
+  req.PRINTED_DT
 FROM CRT_CUST_MAST crtMast
 join CRT_CFG_TYPE crtCfg on crtCfg.cert_ty = crtMast.cert_ty
+left join crt_corr_request req on req.requesting_period = crtMast.period_serno
 /
